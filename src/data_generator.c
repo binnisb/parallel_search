d0a4303d167677cf23bd38330d1f660e2c357628
Sindri <sim20@hi.is>
2012-08-31 16:44:03 +0200

    added the newest stuff

--- src/data_generator.c
+++ src/data_generator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> /* required for randomize() and random() */
 #include <string.h>
-
+#include <time.h>
 
 void random_generatior(char* file_name,int nr_of_lines, int length_of_seq){
   // file_name: string with suffix .txt.  
@@ -32,44 +32,60 @@ void random_generatior(char* file_name,int nr_of_lines, int length_of_seq){
 
 
 
-void read_file(char* file_name, char* key){
-  //  file_name: is direction to a data file.
-  //  key:       the search string.
-  //
+void read_file(char* file_name, char* key, int result_size_block){
+  //  file_name:   is direction to a data file.
+  //  key:         the search string.
+  //  iresult_size_block: is the block size of the array that 
+  //                     contains the resutls which contains the id 
+  //                     of the mathing data. 
   // 
   //
-
 
   // open up file
   FILE *file;
   file = fopen(file_name,"r");
-
   char line[128]; // line in file
 
 
-  char* pch_id; // first column in the current line, corresponds to line id
+  int pch_id; // first column in the current line, corresponds to line id
   char* pch_seq; // second column in the current file, corresponds to the seq in this line
 
-  int result[5000];
-  int i = 0;
+  
+  int result_size = result_size_block; // initualized result size
+  int *result;
+  result = malloc(sizeof(*result)*result_size);
+  int result_counter = 0;
 
   while ( fgets( line, sizeof(line),file) != NULL){
-    pch_id = (int)* (strtok(line,"\t\n")-'0');
+    printf("line=%s\n",line);
+    pch_id = atoi(strtok(line,"\t\n"));
     pch_seq = strtok(NULL,"\t\n");
-    //printf("key=%i, seq=%i \n",(int)strlen(key),(int)strlen(pch_seq));
+    //printf("pch_id0%i, pch_seq=%s\n",pch_id,pch_seq);
     if ( strcmp(pch_seq, key) == 0){
-      printf("ok\n");
-      result[i] = pch_id;  
-      i++;
+      printf("pch_id=%i, pch_seq=%s, result_counter=%i\n",pch_id,pch_seq,result_counter);
+/*      if (result_size == result_counter ){
+        result_size = result_size + result_size_block;
+        result = realloc(result, result_size*sizeof(*result) );
+        if (result == NULL){
+          printf("Error reallocating memory\n");
+          exit(1);
+        }
+      }
+*/      result[result_counter] = pch_id;  
+      printf("ok1\n");
+      result_counter++;
+      printf("ok1\n");
     }
     //fputs(line,stdout);
   }
   fclose(file);
 
 
+
+  // print out results:
   int j;
-  for (j=0;j<i;j++){
-    printf("result[%i]=%i \n",j,result[j]);
+  for (j=0;j<result_counter;j++){
+    //printf("result[%i]=%i \n",j,result[j]);
   }
 
 }
@@ -77,6 +93,26 @@ void read_file(char* file_name, char* key){
 
 int main( int argc, const char* argv[] )
 {
-  random_generatior(5000,20);
-  read_file("file.txt","32588910672835001113"); 
+  time_t start,end;
+  double dif;
+
+  int i;
+
+  char* file_name = "../data/file.txt";
+  random_generatior(file_name,4,4);
+
+  //read_file(file_name,"6942264128",100);
+
+  int rec[3];
+  rec[0] = 1;
+  rec[1] = 1000;
+  rec[2] = 1000000;
+/*
+  for ( i=0;i<3;i++){
+    time (&start);
+    read_file(file_name,"1234567890",rec[i]);
+    time (&end);
+    dif = difftime(end,start);
+    printf("For rec=%i we get time=%f\n,stime=%f,etime=%f\n",rec[i],dif,(double)start,(double)end); 
+  }*/ 
 }
