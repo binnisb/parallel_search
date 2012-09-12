08de30b7cc58369e37964fbc7511911270b975f1
Brynjar Smari Bjarnason <binni@binnisb.com>
2012-09-12 11:18:28 +0200

    data_generator with zero padding, Make is still a problem

--- src/data_generator.c
+++ src/data_generator.c
@@ -14,14 +14,20 @@ void random_generatior(char* file_name,int nr_of_lines, int length_of_seq){
   //          Where id is the id of the line and seq is a sequence of length
   //          length_of_seq of integers.
   //        
-  int i, j, R;
+  int i, j,k, R, padding;
 
   // open up file
   FILE *file; 
   file = fopen(file_name,"w");
-
+  char buf[1000];
+  snprintf(buf, 10,"%d",nr_of_lines);
+  padding = strlen(buf);
   for (i=0;i<nr_of_lines;i++){
-    fprintf(file,"%i\t",i);
+    snprintf(buf, 10,"%d",i);
+    for (k=0;k < padding - (int)strlen(buf);k++){
+      fprintf(file,"0");
+    }
+      fprintf(file,"%i\t",i);
     for (j=0;j<length_of_seq;j++){
       fprintf(file,"%i",rand()%10);  
     }
@@ -41,9 +47,7 @@ int main( int argc, const char* argv[] )
   int data_size = 100000000;
   int string_size = 6;
   char* file_name = "../data/file.txt";
-  char buf [1000];
-  itoa(data_size,buf,1)
-  //random_generatior(file_name,data_size,string_size);
+  random_generatior(file_name,data_size,string_size);
 
   printf("File created: %s\n", file_name);
 }
