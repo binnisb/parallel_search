dcc2e78727790fc65fd2579a2e1f9c7b1ac8d286
smagnsson <sim20@hi.is>
2012-10-26 12:11:40 +0200

    ji

--- src/search_mpio.c
+++ src/search_mpio.c
@@ -11,24 +11,36 @@ long long ae_load_file_to_memory(MPI_File   infile,
                                  long long  nr_lines,
                                  int        line_size) 
 {
-  //  filename:		path to the file to read
-  //  result:		pointer to character array that contains the content of the file. 
-  //  my_rank:          my rank number
-  //  numprocs:         number of processes
+  //  infile:      the data file
+  //  result:      pointer to character array that contains the content of the file. 
+  //  my_rank:     my rank number
+  //  numprocs:    number of processes
+  //  nr_lines:    nr of lines in file       
+  //  line_size:   line size in the data file
+  //
+  //  putts the data the process my_rank is suppose to search in into resutls.
+  //  returns the size of resutls in bytes.
 
-  MPI_Status stat;
-		
-  long long my_size = (nr_lines+my_rank)/numprocs; // number of lines the current process will read
-        
-  *result = (char *)malloc(line_size*my_size);
+  MPI_Status stat;		
+
+  // number of lines the current process will read
+  long long my_size = (nr_lines+my_rank)/numprocs;        
+
+  // allocate memory for result
+  *result = (char *)malloc(line_size*my_size); 
+
+  // the first line this process will read:
   MPI_Offset start_read = my_rank*( nr_lines/numprocs + my_rank/(numprocs-nr_lines%numprocs+1));
-  //MPI_File_set_view( infile, start_read, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL );
+
+  // Set the pointer to the first line this process will read:
   MPI_File_seek(infile, start_read*line_size, MPI_SEEK_SET); 
+
+  // Read lines start_read,start_read+1,...,start_read+my_size-1 to result:
   MPI_File_read( infile, *result, line_size*my_size, MPI_CHAR, &stat );
 
+  // Close the 
   MPI_File_close(&infile);
 
-  printf("start_read: %lli \n",start_read);
   return line_size*my_size;
 }
 
@@ -41,8 +53,8 @@ int read_file(char*      input_file,
   //  input_file:        is direction to a data file.
   //  key:               the search string.
   //  result_size_block: is the block size of the array that 
-  //                     contains the resutls which contains the id 
-  //                     of the mathing data. 
+  //                     contains the resutls, the id's 
+  //                     of the mathing data.
   //  nr_lines:          nr of lines in file. 
   //  line_sie:          size of line in char
   //
@@ -84,9 +96,9 @@ int read_file(char*      input_file,
 int main( int argc,  char* argv[] )
 {
 
-  // Paralell starts:
-  int numprocs; // number of processes
-  int myid;     // my rank
+  int numprocs;  // number of processes
+  int myid;      // my rank
+
   MPI_Status stat;
   /* MPI programs start with MPI_Init; all 'N' processes exist thereafter */
   MPI_Init(&argc,&argv);
@@ -95,89 +107,69 @@ int main( int argc,  char* argv[] )
   /* and this processes' rank is */
   MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
+  MPI_File infile; // the data file
+
+  // Variables to measure time performance
   double start,end;
   double dif;
   
-  char* file_name = "../data/file.txt";
+  char* file_name = "../data/file.txt"; // name of the data file
+  char* search_key = "123123";          // the search key
+  // An array of char's that contains all the lines from infile that this 
+  // process is responseble to search in:
   char* result;
-  char* search_key = "123123";
 
-  long long nr_bytes; 
-  long long i;
-  long long nr_lines;
+  long long nr_lines; // nr of lines in the data file 
+  long long nr_bytes; // nr of bytes in th 
+  long long size;     // nr of bytes in the data file
 
   int string_size = strlen(search_key);
-  int block_size; //
+  int block_size; // 
   int read_count; // result: number of found elements
-  int line_size;  // line size
-  int ierr, sum;
+  int line_size;  // line size in the data file
+  int ierr, sum;  
 
   // Find file size in bytes:
-  long long size = 0;
-  //FILE *f = fopen(file_name, "rb");
-  MPI_File infile;
+  size = 0;
   MPI_File_open( MPI_COMM_WORLD, file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &infile );
-
-//  if (infile == NULL)
-//  {
-//    return -1; // -1 means file opening fail 
-//  }
- 
-  MPI_File_get_size(infile,&size); // number of bytes in file
-  // fseek(f, 0, SEEK_SET);
+  MPI_File_get_size(infile,&size); // get number of bytes in the data file
   
-
   // Find the line size in byte:
   char temp_read[1000];
   MPI_File_set_view( infile, 0, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL );
   MPI_File_read( infile, &temp_read, 1000, MPI_CHAR, &stat );  
-  //fread(temp_read, sizeof(char), 1000 , f);
   char* pchr = strchr( temp_read, '\n');
   line_size = pchr-temp_read+1; 
-
-
-//  MPI_File_close(&infile);
-//  MPI_Finalize();
-//  exit(0);
 
   // get number of lines:
   nr_lines = size/line_size;
   
+  // put the part of the data file that this process is responseble
+  // for searching in into results:
   start = MPI_Wtime();
   nr_bytes = ae_load_file_to_memory(infile ,&result, myid, numprocs, nr_lines, line_size);
-
-  
-
-  nr_lines = nr_bytes / line_size;
-/*
-  int ii;
-  char line[20];
-  for ( ii=0 ; ii!= nr_lines ; ii++){
-    strncpy(line,&result[ii*line_size],line_size); 
-    if ( ii==0 ){
-      printf("my_rank: %i, first: %s \n",myid,line);
-    }
-  } 
-  printf("my_rank: %i, last: %s \n",myid,line);
-*/
   end = MPI_Wtime();
   dif = end-start;
-  //printf("LoadFile: %f\n", dif);
+  printf("LoadFile: %f\n", dif);  
+
+  // nr of lines in results
+  nr_lines = nr_bytes / line_size; 
 
   block_size = 1.5*(nr_lines/pow(1,string_size));
-  start = MPI_Wtime();
 
+  // search in the part of the data this process is responseble for:
+  start = MPI_Wtime();
   read_count = read_file(result,search_key,block_size,nr_lines,line_size);
   end = MPI_Wtime();
   dif = end - start;
-  //printf("Search: %f\n",dif);
+  printf("Search: %f\n",dif);
+  
   ierr = MPI_Reduce(&read_count, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if(myid == 0) {
     printf("The sum is %i\n", sum);
   } 
 
-//  MPI_File_close(&infile);
   MPI_Finalize();
   return 0;
 
